a6f4e1653478a58fdf1a46f3c6ba379ecc20bee6
David Thomas <thodl15@gmail.com>
2020-08-14 18:57:28 -0400

    (Update) Complete Flask Setup

---
+++ jobs/app.py
@@ -0,0 +1,11 @@
+# STL Imports
+
+# Third Party Imports
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/jobs')
+def jobs():
+    return render_template('index.html')
