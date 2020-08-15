f9c33dc6d9bf46ca3e44a2ca3b10d6b6880bb0ab
David Thomas <thodl15@gmail.com>
2020-08-14 20:21:43 -0400

    (Update) Complete Display All Jobs Module

--- jobs/app.py
+++ jobs/app.py
@@ -45,4 +45,6 @@ def execute_sql(sql, values = (), commit = False, single = False):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    query = 'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id'
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
